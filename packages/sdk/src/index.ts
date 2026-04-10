65232c85d955abe1f00dfbd7f5fa61b26a32a9cd
0xpulse <0xpulse@trace-protocol.xyz>
2026-04-10 10:09:41 +0000

    feat(sdk): public api re-export

---
+++ packages/sdk/src/index.ts
@@ -0,0 +1,12 @@
+export { TraceClient, TraceApiError } from "./client";
+export type {
+  AgentVerdict,
+  VerdictResponse,
+  VerdictReasonInfo,
+  BatchQueryRequest,
+  BatchQueryOptions,
+  BatchQueryResponse,
+  TraceClientConfig,
+  FeedMessage,
+  VerdictCallback,
+} from "./types";
